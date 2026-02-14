17ed5cf34ea10ef7fb861dbdc6f3d779f19202b4
matteo-giustini <matteo.giustini3@gmail.com>
2026-02-14 13:14:47 +0100

    feat add front

---
+++ front_end/app.js
@@ -0,0 +1,12 @@
+const loginElm = document.getElementById ('loginForm');
+const registerElm = document.getElementById('registerForm');
+const message = document.getElementById('message');
+
+if (loginElm) {
+    loginElm.addEventListener('submit', function (e)) {
+        e.preventDefault();
+
+        const email = document.getElementById('email').value;
+        const password = 
+    }
+}
