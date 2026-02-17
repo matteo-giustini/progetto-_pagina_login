65be09c3b46174ef67af47993427a590c012c61f
matteo-giustini <matteo.giustini3@gmail.com>
2026-02-17 16:20:03 +0100

    feat add backend dir

--- front_end/app.js
+++ front_end/app.js
@@ -3,10 +3,25 @@ const registerElm = document.getElementById('registerForm');
 const message = document.getElementById('message');
 
 if (loginElm) {
-    loginElm.addEventListener('submit', function (e)) {
-        e.preventDefault();
+    loginElm.addEventListener('submit', function (e) {
+    e.preventdefault();
 
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    console.log("Login:", email, password);
+
+    message.txtContent = "login simulato";
+    })
+}
+
+if (registerElm) {
+    registerElm.addEventListener('submit', functiion (e) {
         const email = document.getElementById('email').value;
-        const password = 
-    }
+        const password = document.getElementById('password').value;
+
+        console.log("Login:", email, password);
+
+    message.txtContent = "register simulato";
+    })
 }
